320de56b48f3ff405bc3ffea4de756e9b884bd3d
Petro Arva <petroarva33@gmail.com>
2021-10-17 23:32:48 +0200

    Added minutes to the project

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -5,16 +5,20 @@ const btnStop = document.querySelector(".btn-stop");
 const btnReset = document.querySelector(".btn-reset");
 
 let interval; // COMMENT: works only globally
-let seconds, tens;
+let seconds, tens, minutes;
 
 function init() {
   const secondsEl = document.querySelector(".stopWatch__seconds");
   const tensEl = document.querySelector(".stopWatch__tens");
+  const minutesEl = document.querySelector(".stopWatch__minutes");
+
   clearInterval(interval);
+  minutesEl.textContent = `00`;
   secondsEl.textContent = `00`;
   tensEl.textContent = `00`;
   seconds = 0;
   tens = 0;
+  minutes = 0;
 }
 
 init();
@@ -31,12 +35,20 @@ function stopInterval() {
 function runTimer() {
   const secondsEl = document.querySelector(".stopWatch__seconds");
   const tensEl = document.querySelector(".stopWatch__tens");
+  const minutesEl = document.querySelector(".stopWatch__minutes");
   tens++;
   if (tens > 99) {
     tens = 0;
     ++seconds;
     secondsEl.textContent = seconds < 10 ? `0${seconds}` : seconds;
   }
+
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    minutesEl.textContent = minutes < 10 ? `0${minutes}` : minutes;
+  }
+
   tensEl.textContent = tens < 10 ? `0${tens}` : tens;
 }
 
