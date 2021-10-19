29617412660c024f898a59f9c62876bc8e16dd3b
Petro Arva <petroarva33@gmail.com>
2021-10-19 18:30:18 +0200

    Changed setInterval to setTimeout

--- trafficLight/script.js
+++ trafficLight/script.js
@@ -7,33 +7,62 @@ let redInterval;
 let yellowInterval;
 let greenInterval;
 
-function activeRed() {
-  redInterval = setInterval(function () {
-    redEl.classList.add("active-red");
-    greenEl.classList.remove("active-green");
-  }, 3800);
-}
-
-activeRed();
-
-function activeYellow() {
-  yellowInterval = setInterval(() => {
-    yellowEl.classList.add("active-yellow");
-  }, 5300);
-}
-
-activeYellow();
-
-function activeGreen() {
-  greenInterval = setInterval(() => {
-    greenEl.classList.add("active-green");
-    redEl.classList.remove("active-red");
-    yellowEl.classList.remove("active-yellow");
-    clearInterval(redInterval);
-    clearInterval(yellowInterval);
+function startTrafficLight() {
+  redInterval = setTimeout(() => {
     activeRed();
+  }, 3800);
+
+  yellowInterval = setTimeout(() => {
     activeYellow();
+  }, 6000);
+
+  greenInterval = setTimeout(() => {
+    activeGreen();
   }, 7500);
 }
 
-activeGreen();
+let interval = setInterval(() => {
+  startTrafficLight();
+}, 8000);
+
+function activeRed() {
+  // redInterval = setInterval(function () {
+  //   redEl.classList.add("active-red");
+  //   greenEl.classList.remove("active-green");
+  // }, 3800);
+  redEl.classList.add("active-red");
+  greenEl.classList.remove("active-green");
+}
+
+// activeRed();
+
+function activeYellow() {
+  // yellowInterval = setInterval(() => {
+  //   yellowEl.classList.add("active-yellow");
+  // }, 5300);
+
+  yellowEl.classList.add("active-yellow");
+}
+
+// activeYellow();
+
+function activeGreen() {
+  // greenInterval = setInterval(() => {
+  //   greenEl.classList.add("active-green");
+  //   redEl.classList.remove("active-red");
+  //   yellowEl.classList.remove("active-yellow");
+  //   clearInterval(redInterval);
+  //   clearInterval(yellowInterval);
+  //   activeRed();
+  //   activeYellow();
+  // }, 7500);
+
+  greenEl.classList.add("active-green");
+  redEl.classList.remove("active-red");
+  yellowEl.classList.remove("active-yellow");
+
+  // activeRed();
+  // activeYellow();
+}
+
+// activeGreen();
