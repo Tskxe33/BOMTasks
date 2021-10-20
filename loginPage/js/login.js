a6b239ad3c27b1d19bacda0021f8f76658014dda
Petro Arva <petroarva33@gmail.com>
2021-10-20 23:52:24 +0200

    Added logic for login page

--- loginPage/js/login.js
+++ loginPage/js/login.js
@@ -1,7 +1,48 @@
 "use strict";
 
-const form = document.querySelector(".aside__form");
+// const rememberMe = document.getElementById("rememberMe");
+const loginForm = document.querySelector(".form__login");
 
-form.addEventListener("submit", function (e) {
+let loggedIn = false;
+
+function displayErrorMessage() {
+  const message = document.querySelector(".error-message");
+  message.classList.add("animate__fadeInLeft");
+  message.style.display = "block";
+}
+
+loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  getUser().then((res) =>
+    res
+      ? location.replace("http://127.0.0.1:5500/loginPage/home.html")
+      : displayErrorMessage()
+  );
 });
+
+function fetchUsers() {
+  const usersURL = "/loginPage/users.json";
+  return fetch(usersURL).then((response) => response.json());
+}
+
+function getUser() {
+  const password = document.getElementById("password");
+  const username = document.getElementById("username");
+  return fetchUsers().then((res) => {
+    return res.users.find((user) => {
+      if (
+        user.username === username.value.trim() &&
+        user.password === password.value
+      ) {
+        document.querySelector(".error-message").style.display = "none";
+        return user;
+      }
+    });
+  });
+}
+
+document
+  .getElementById("forgotPassword")
+  .addEventListener("click", function () {
+    location.replace("http://127.0.0.1:5500/loginPage/resetPassword.html");
+  });
