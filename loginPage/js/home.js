5b08f8b3678b952245e9c678214d0f1c4eee430d
Petro Arva <petroarva33@gmail.com>
2021-10-20 23:51:50 +0200

    Added hander on logout btn

---
+++ loginPage/js/home.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const logout = document.getElementById("logout");
+
+logout.addEventListener("click", function () {
+  location.replace("http://127.0.0.1:5500/loginPage/index.html");
+});
