8d20bda435204a29807388a8868247e74735dc2f
Petro Arva <petroarva33@gmail.com>
2021-10-20 23:53:08 +0200

    Added handler on go back btn

---
+++ loginPage/js/resetPass.js
@@ -0,0 +1,5 @@
+"use strict";
+document.getElementById("goBack").addEventListener("click", function (e) {
+  e.preventDefault();
+  location.replace("http://127.0.0.1:5500/loginPage/index.html");
+});
