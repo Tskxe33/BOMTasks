343ec0a89d3710704dda8a6ea63151b5c058e342
Petro Arva <petroarva33@gmail.com>
2021-10-21 19:53:38 +0200

    Added cookies module

---
+++ loginPage/js/modules/cookies.js
@@ -0,0 +1,32 @@
+export function saveCookie(name, value) {
+  document.cookie = `${name}=${value}`;
+}
+
+export function getCookieValue(cookieName) {
+  let cookie = document.cookie
+    .split("; ")
+    .find((name) => name.startsWith(cookieName));
+
+  return cookie;
+}
+
+function expiryDate() {
+  const date = new Date();
+  date.setMonth(date.getMonth() + 1);
+
+  return date;
+}
+
+export function rememberCookie(name, value) {
+  document.cookie = `${name}=${value}; expires=${expiryDate().toUTCString()}`;
+}
+
+function yesterday() {
+  const date = new Date();
+  date.setDate(date.getDate() - 1);
+  return date;
+}
+
+export function removeCookie(name) {
+  document.cookie = `${name}=; expires=${yesterday().toUTCString()}`;
+}
