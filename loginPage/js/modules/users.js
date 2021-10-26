ad763551fa32a2a230807dfc8582bfc2bfdef992
Petro Arva <petroarva33@gmail.com>
2021-10-26 22:21:58 +0200

    Added new functions to module

--- loginPage/js/modules/users.js
+++ loginPage/js/modules/users.js
@@ -1,20 +1,34 @@
+import * as cookies from "../modules/cookies.js";
+import * as pathNames from "../modules/pathNames.js";
+
 function fetchUsers() {
   const usersURL = "/loginPage/users.json";
   return fetch(usersURL).then((response) => response.json());
 }
 
-export function getUser() {
-  const password = document.getElementById("password"); //parametre ubaciti
-  const username = document.getElementById("username");
+export function getUser(password, username) {
   return fetchUsers().then((res) => {
     return res.users.find((user) => {
       if (
         user.username === username.value.trim() &&
         user.password === password.value
-      ) {
-        document.querySelector(".error-message").style.display = "none"; //
+      )
         return user;
-      }
     });
   });
+}
+
+export function userToHome() {
+  document.cookie
+    ? (location.href = `${pathNames.origin}${pathNames.homePathName}`)
+    : "";
+}
+
+export function checkIfUserLoggedIn() {
+  if (
+    !cookies.getCookieValue("userID") &&
+    location.href == `${pathNames.origin}${pathNames.homePathName}` //
+  ) {
+    location.href = `${pathNames.origin}${pathNames.loginPathName}`;
+  }
 }
